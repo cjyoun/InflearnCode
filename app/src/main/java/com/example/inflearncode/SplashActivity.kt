3f60d9bcf149011529e24819f8aa8e0f9b8d0fb1
cjyoun <cjyoun95@naver.com>
2022-07-05 23:15:56 +0900

    Splash화면에서 2초뒤에 MainActivity로 이동

--- app/src/main/java/com/example/inflearncode/SplashActivity.kt
+++ app/src/main/java/com/example/inflearncode/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.example.inflearncode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // 핸들러를 사용하여 2초뒤에 MainActivity로 이동
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+
     }
 }
