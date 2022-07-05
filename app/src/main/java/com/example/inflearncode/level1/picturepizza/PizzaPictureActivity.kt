03b6c0d7acbc7e765c8c11fb93ee745f21d19e84
cjyoun <cjyoun95@naver.com>
2022-07-06 08:16:52 +0900

    피자사진화면 생성

---
+++ app/src/main/java/com/example/inflearncode/level1/picturepizza/PizzaPictureActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inflearncode.level1.picturepizza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inflearncode.R
+
+class PizzaPictureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pizza_picture)
+    }
+}
