9afe92af23b11277d836b8206edd5ce9326197a8
cjyoun <cjyoun95@naver.com>
2022-07-05 22:32:28 +0900

    메인 화면 추가

--- app/src/main/java/com/example/inflearncode/MainActivity.kt
+++ app/src/main/java/com/example/inflearncode/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.inflearncode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val goLevel1 = findViewById<Button>(R.id.goLevel1)
+
+        goLevel1.setOnClickListener{
+            //val goLevel1Activity = Intent(this, )
+        }
+
+
     }
 }
