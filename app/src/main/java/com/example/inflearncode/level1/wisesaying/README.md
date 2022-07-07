bcc657a61502407c707e5637cbce6fab71485381
cjy <cjyoun95@naver.com>
2022-07-07 18:01:54 +0900

    명언 리스트 앱 / 폰트 추가

---
+++ app/src/main/java/com/example/inflearncode/level1/wisesaying/README.md
@@ -0,0 +1,4 @@
+# 폰트 넣기 
+res 안에 font Directory 생성해서 폰트 넣기
+xml text에서 fontFamily="@font/폰트파일명" 으로 가져오기
+
---
+++ app/src/main/java/com/example/inflearncode/level1/wisesaying/WiseSayingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.inflearncode.level1.wisesaying
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inflearncode.R
+
+class WiseSayingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wise_saying)
+    }
+}
