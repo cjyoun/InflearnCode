3f59894766564e34928f73db543f24dd095202ee
cjyoun <cjyoun95@naver.com>
2022-07-12 22:52:37 +0900

    다이어트 앱 / firebase realtime database에 저장하기

--- app/src/main/java/com/example/inflearncode/level1/dietmemo/DietMemoActivity.kt
+++ app/src/main/java/com/example/inflearncode/level1/dietmemo/DietMemoActivity.kt
@@ -7,9 +7,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 import com.example.inflearncode.R
+import com.example.inflearncode.level1.dietmemo.model.DataModel
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.time.DayOfWeek
 import java.time.Year
 import java.util.*
@@ -29,10 +33,13 @@ class DietMemoActivity : AppCompatActivity() {
 
             val mAlertDialog = mBuilder.show()
 
-            val DateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+            var dateText = ""
+            // custom dialog에 있는 id값에 해당되는 것 가져오기
+            val dateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
             // date picker 보이기
-            DateSelectBtn?.setOnClickListener {
+            dateSelectBtn?.setOnClickListener {
 
+                // 날짜 선택하기
                 val today = GregorianCalendar()
                 val year : Int = today.get(Calendar.YEAR)
                 val month : Int = today.get(Calendar.MONTH)
@@ -42,12 +49,37 @@ class DietMemoActivity : AppCompatActivity() {
                     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfWeek: Int) {
                         Log.d("DietMemo-cutomDialog", "$year , ${month+1} , $dayOfWeek")
 
-                        DateSelectBtn.text = "$year - ${month+1} - $dayOfWeek"
+                        dateSelectBtn.text = "$year - ${month+1} - $dayOfWeek"  // 버튼의 text 값 변경
+
+                        dateText = "$year - ${month+1} - $dayOfWeek"    // 날짜 값
 
                     }
                 }, year, month, day)
+                dlg.show()
 
             }
+
+
+            // firebase database 저장
+            val saveBtn = mAlertDialog.findViewById<Button>(R.id.dateSaveBtn)
+
+            saveBtn?.setOnClickListener {
+
+                val healthMemo = mAlertDialog.findViewById<EditText>(R.id.healthMemo).toString()   // 메모 내용
+
+                // firebase database
+                val database = Firebase.database
+                val myRef = database.getReference("message")
+
+//                myRef.setValue("~~~~")  // database에 데이터 넣기 (중복된 데이터는 더 쌓이지 않음)
+//                myRef.push().setValue("!!!!!")  // database에 중복되더라도 입력되는대로 데이터 넣기
+
+                val model = DataModel(dateText, healthMemo)
+                myRef.push().setValue(model)    // 모델값 database에 저장하기
+
+            }
+
+
 
         }
 
