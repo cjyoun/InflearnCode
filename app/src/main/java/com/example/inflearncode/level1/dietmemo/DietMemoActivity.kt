74b6fde2f0579ae5c1cbf299ba41512e1462c708
cjy <cjyoun95@naver.com>
2022-07-12 18:07:53 +0900

    다이어트 메모앱 / 커스텀다이얼로그 생성

--- app/src/main/java/com/example/inflearncode/level1/dietmemo/DietMemoActivity.kt
+++ app/src/main/java/com/example/inflearncode/level1/dietmemo/DietMemoActivity.kt
@@ -1,9 +1,18 @@
 package com.example.inflearncode.level1.dietmemo
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import com.example.inflearncode.R
+import java.time.DayOfWeek
+import java.time.Year
+import java.util.*
 
 class DietMemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +23,33 @@ class DietMemoActivity : AppCompatActivity() {
         val writeBtn = findViewById<ImageView>(R.id.writeBtn)
         writeBtn.setOnClickListener {
 
-        }
+            // 다이얼로그 출력하기
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.cutom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this).setView(mDialogView).setTitle("운동 메모 다이얼로그")
 
+            val mAlertDialog = mBuilder.show()
+
+            val DateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+            // date picker 보이기
+            DateSelectBtn?.setOnClickListener {
+
+                val today = GregorianCalendar()
+                val year : Int = today.get(Calendar.YEAR)
+                val month : Int = today.get(Calendar.MONTH)
+                val day : Int = today.get(Calendar.DATE)
+
+                val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfWeek: Int) {
+                        Log.d("DietMemo-cutomDialog", "$year , ${month+1} , $dayOfWeek")
+
+                        DateSelectBtn.text = "$year - ${month+1} - $dayOfWeek"
+
+                    }
+                }, year, month, day)
+
+            }
+
+        }
 
     }
 }
