a217c30b60ac618764e4402fb7cdc8351f8b8b4b
cjyoun <cjyoun95@naver.com>
2022-08-07 21:56:30 +0900

    navigation 이동

--- app/src/main/java/com/example/inflearncode/level2/view/fragment/CommunityBookmarkFragment.kt
+++ app/src/main/java/com/example/inflearncode/level2/view/fragment/CommunityBookmarkFragment.kt
@@ -1,7 +1,6 @@
 package com.example.inflearncode.level2.view.fragment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,12 +8,12 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.inflearncode.R
-import com.example.inflearncode.databinding.FragmentCommunityHomeBinding
+import com.example.inflearncode.databinding.FragmentCommunityBookmarkBinding
 
 
-class CommunityHomeFragment : Fragment() {
+class CommunityBookmarkFragment : Fragment() {
 
-    private lateinit var binding : FragmentCommunityHomeBinding
+    private lateinit var binding: FragmentCommunityBookmarkBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,34 +25,30 @@ class CommunityHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_community_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_community_bookmark, container, false)
 
-        // 북마크 fragment로 이동
-        binding.bookMarkTap.setOnClickListener {
+        // 홈 fragment로 이동
+        binding.homeTap.setOnClickListener {
             // 네비게이션으로 이동
-            it.findNavController().navigate(R.id.action_communityHomeFragment_to_communityBookmarkFragment)
-
+            it.findNavController().navigate(R.id.action_communityBookmarkFragment_to_communityHomeFragment)
         }
 
         // 꿀팁 fragment로 이동
         binding.tipTap.setOnClickListener {
             // 네비게이션으로 이동
-            it.findNavController().navigate(R.id.action_communityHomeFragment_to_communityTipFragment)
-
+            it.findNavController().navigate(R.id.action_communityBookmarkFragment_to_communityTipFragment)
         }
 
         // 스토어 fragment로 이동
         binding.storeTap.setOnClickListener {
             // 네비게이션으로 이동
-            it.findNavController().navigate(R.id.action_communityHomeFragment_to_communityStoreFragment)
-
+            it.findNavController().navigate(R.id.action_communityBookmarkFragment_to_communityStoreFragment)
         }
 
         // talk fragment로 이동
         binding.talkTap.setOnClickListener {
             // 네비게이션으로 이동
-            it.findNavController().navigate(R.id.action_communityHomeFragment_to_communityTalkFragment)
-
+            it.findNavController().navigate(R.id.action_communityBookmarkFragment_to_communityTalkFragment)
         }
 
 
--- app/src/main/java/com/example/inflearncode/level2/view/fragment/CommunityTipFragment.kt
+++ app/src/main/java/com/example/inflearncode/level2/view/fragment/CommunityTipFragment.kt
@@ -5,56 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.inflearncode.R
+import com.example.inflearncode.databinding.FragmentCommunityStoreBinding
+import com.example.inflearncode.databinding.FragmentCommunityTipBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CommunityTipFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CommunityTipFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentCommunityTipBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community_tip, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_community_tip, container,false)
+
+
+        // 홈 fragment로 이동
+        binding.homeTap.setOnClickListener {
+            // 네비게이션으로 이동
+            it.findNavController().navigate(R.id.action_communityTipFragment_to_communityHomeFragment)
+        }
+
+        // talk fragment로 이동
+        binding.talkTap.setOnClickListener {
+            // 네비게이션으로 이동
+            it.findNavController().navigate(R.id.action_communityTipFragment_to_communityTalkFragment)
+        }
+
+        // 스토어 fragment로 이동
+        binding.storeTap.setOnClickListener {
+            // 네비게이션으로 이동
+            it.findNavController().navigate(R.id.action_communityTipFragment_to_communityStoreFragment)
+        }
+
+        // 북마크 fragment로 이동
+        binding.bookMarkTap.setOnClickListener {
+            // 네비게이션으로 이동
+            it.findNavController().navigate(R.id.action_communityTipFragment_to_communityBookmarkFragment)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CommunityTipFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CommunityTipFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
