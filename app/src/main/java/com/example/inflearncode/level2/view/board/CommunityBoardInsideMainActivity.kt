4e9221bc9be3ae2c92ad5a9a961608b07146b247
cjyoun <cjyoun95@naver.com>
2022-08-10 16:16:55 +0900

    게시판 - 갤러리 호출 , 이미지 firebase storage 저장 및 호출

--- app/src/main/java/com/example/inflearncode/level2/view/board/CommunityBoardInsideMainActivity.kt
+++ app/src/main/java/com/example/inflearncode/level2/view/board/CommunityBoardInsideMainActivity.kt
@@ -3,14 +3,19 @@ package com.example.inflearncode.level2.view.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.inflearncode.R
 import com.example.inflearncode.databinding.ActivityCommunityBoardInsideMainBinding
 import com.example.inflearncode.databinding.ActivityCommunityBoardWriteBinding
 import com.example.inflearncode.level2.util.FirebaseRef
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class CommunityBoardInsideMainActivity : AppCompatActivity() {
 
@@ -37,6 +42,9 @@ class CommunityBoardInsideMainActivity : AppCompatActivity() {
         val key = intent.getStringExtra("key").toString()
         getBoardData(key)
 
+        // 이미지 가져오기
+        getBoardImageData(key)
+
     }
 
     private fun getBoardData(key: String){
@@ -62,5 +70,24 @@ class CommunityBoardInsideMainActivity : AppCompatActivity() {
 
     }
 
+    // 이미지 가져오기
+    private fun getBoardImageData(key: String){
+        // Reference to an image file in Cloud Storage
+        val storageReference = Firebase.storage.reference.child("$key.png")
+
+        // ImageView in your Activity
+        val imageView = binding.boardImgArea
+
+        storageReference.downloadUrl.addOnCompleteListener (OnCompleteListener{ task ->
+            if(task.isSuccessful){
+                Glide.with(this)
+                    .load(task.result)
+                    .into(imageView)
+            }else{
+
+            }
+        })
+
+    }
 
 }
--- app/src/main/java/com/example/inflearncode/level2/view/board/CommunityBoardWriteActivity.kt
+++ app/src/main/java/com/example/inflearncode/level2/view/board/CommunityBoardWriteActivity.kt
@@ -1,18 +1,27 @@
 package com.example.inflearncode.level2.view.board
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.inflearncode.R
 import com.example.inflearncode.databinding.ActivityCommunityBoardWriteBinding
 import com.example.inflearncode.level2.util.FirebaseAuth
 import com.example.inflearncode.level2.util.FirebaseRef
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
 
 class CommunityBoardWriteActivity : AppCompatActivity() {
 
     private val TAG = CommunityBoardWriteActivity::class.java.simpleName
     private lateinit var binding : ActivityCommunityBoardWriteBinding
+
+    private var isImageUpload = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,16 +36,79 @@ class CommunityBoardWriteActivity : AppCompatActivity() {
             val uid = FirebaseAuth.getUid()
             val time = FirebaseAuth.getTime()
 
+            // firebase storage에 이미지를 저장하려고 할 때
+            // 게시글을 선택하면 그 게시글의 데이터를 가져오는 것 처럼 이미지도 가져오기 위해서
+            // 게시글을 저장할 때 생성되는 key 값을 가지고 있고 image 생성시 그 키값을 파일명으로 해서 저장
+            // (이미지 이름을 모르기 때문에 key값으로 저장시키는 것)
+
+            // 랜덤한 키값
+            val key = FirebaseRef.boardRef.push().key.toString()
+
+            // firebase에 게시글 데이터 저장 -> 키값으로 push 됨
             FirebaseRef.boardRef
-                .push() // 랜덤한 키값으로 push 됨
+                .child(key)
                 .setValue(CommunityBoardModel(title,content,uid,time))
 
             Toast.makeText(this,"게시글 등록 완료",Toast.LENGTH_SHORT).show()
+
+            // 이미지 추가 버튼을 눌렀을 경우에만 실행
+            if(isImageUpload){
+                imageUpload(key) // firebase storage에 이미지 업로드 하기
+            }
 
             finish() // 글 등록 후 Activity 끄기
 
         }
 
 
+        // 사진 추가 버튼 클릭 시
+        binding.boardImgBtnArea.setOnClickListener {
+            isImageUpload = true
+            // 갤러리 사진 열기
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, 100)
+
+        }
+
+
     }
+
+    // startActivityForResult 함수 호출 후 결과 내려오는 부분
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RESULT_OK && requestCode ==100){
+            binding.boardImgBtnArea.setImageURI(data?.data)    // 갤러리 선택한 이미지 넣기
+        }
+    }
+
+    // firebase에 image 업로드하기 https://firebase.google.com/docs/storage/android/upload-files?hl=ko
+    private fun imageUpload(key:String){
+        val storage = Firebase.storage // firebase storage
+
+        // Create a storage reference from our app
+        val storageRef = storage.reference
+
+        // 게시글의 키값과 같은 이름으로 사진파일명 지정
+        val mountainsRef = storageRef.child("$key.png")
+
+        // Get the data from an ImageView as bytes
+        val imageView = binding.boardImgBtnArea
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
+    }
+
+
 }
